8817e6224ef761a9d8f523a732bbb2c8ad540fee
maazkhan911 <khanmaaz9111@mail.com>
2025-06-25 21:23:40 +0500

    Initial portfolio upload

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
